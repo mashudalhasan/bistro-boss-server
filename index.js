1db50a9e0800b50b608bc66d398de32d37f3a080
mashudalhasan <mashudalhasan554@gmail.com>
2023-05-30 23:50:40 +0600

    make admin functionality added

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
